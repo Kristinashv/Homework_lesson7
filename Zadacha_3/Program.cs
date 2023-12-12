46017ee798678ae49d744872354701ad24fe3bcf
Kristina Shabovich <Kristina.beloshapko@mail,ru>
2023-12-13 02:14:25 +0300

    add Zadacha 3

---
+++ Zadacha_3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Zadacha 3
+
+int SIZE = 5;
+    int[] arr = new int [SIZE];
+    Console.Write("Введите 1 элемент массива = ");
+    arr[0] = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите 2 элемент массива = ");
+    arr[1] = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите 3 элемент массива = ");
+    arr[2] = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите 4 элемент массива = ");
+    arr[3] = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите 5 элемент массива = ");
+    arr[4] = Convert.ToInt32(Console.ReadLine());
+    fun (arr , SIZE);
+  static int fun(int[] arr , int SIZE)
+  {
+    Console.Write(arr[SIZE-1]+" ");
+    SIZE--;
+    if (SIZE == 0)
+    {
+        return 0;
+    }
+    return fun(arr , SIZE);    
+  }
