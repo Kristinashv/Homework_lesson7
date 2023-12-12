a42acb3cde67caaedc06010146abddba924ffb85
Kristina Shabovich <Kristina.beloshapko@mail,ru>
2023-12-13 01:43:02 +0300

    add Zadacha 1

---
+++ Zadacha_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Zadacha 1
+
+
+    Console.Write("Введите N = ");
+    int N = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите M = ");
+    int M = Convert.ToInt32(Console.ReadLine());
+    fun (N,M);
+  
+  static int fun(int N , int M)
+  {
+      if (N==M)
+      {
+          Console.Write(N + " ");
+          return 0;
+      }
+      Console.Write(N + " ");
+      N++;
+      return fun(N , M);    
+  }
